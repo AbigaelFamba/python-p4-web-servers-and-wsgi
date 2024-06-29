814445939db13cd1d7f4a0cde2d02c6cce344679
Abigael <fambabigael38@gmail.com>
2024-06-29 22:38:28 +0300

    code

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+from flask import Flask request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index();
+  print(f'The web server is up and running at {request.remote_addr}')
+  return 'This response was created by a WSGI!'
+
+if __name__ == '____';
+  app.run(host='localhost' port=5555)
+```
